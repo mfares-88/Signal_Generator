95bbb8d2a7d386c242d2d1eeb4ecea8dcd32af56
mfares <mahmood.fares1988@gmail.com>
2026-07-23 15:38:11 +0300

    chore: prune obsolete state files, VS code history, and build cache artifacts

--- .pio/libdeps/esp32-s3-n4r8/Dev Device Pins/src/PINS_ESP32-S3-MATRIX.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define DEV_DEVICE_PINS
-
-// Button
-#define BTN_A 0
-
-// NeoPixel
-#define NEOPIXEL 14
--- .pio/libdeps/esp32-s3-n4r8/Dev Device Pins/src/PINS_XIAO_ESP32C6.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define DEV_DEVICE_PINS
-
-// LED
-#define LED_A 15
